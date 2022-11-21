dda8ea46d8c135bdccdfb8bd3d3f35f52089e2f4
“Emmett <emmett.wechsler11@gmail.com>
2022-11-21 15:38:36 -0500

    A few enumerables created

---
+++ Enumerables.rb
@@ -0,0 +1,36 @@
+class Array
+
+    def
+
+    def my_each(arr)
+
+        idx = 0
+
+        while idx < (self.length -1)
+            puts self[idx]
+            idx += 1
+        end
+
+        self
+    end
+
+    def my_select(&blk)
+        new_arr = []
+
+        self.my_each { |ele| new_arr << ele if blk.call(ele) }
+
+        new_arr
+    end
+
+    def my_reject(&blk)
+        new_arr = []
+
+        self.my_each { |ele| new_arr << ele if !blk.call(ele) }
+
+        new_arr
+    end
+
+
+
+end
+
