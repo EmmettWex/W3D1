9b2b6a73a66c3e2482842833626c2d534eac685a
chakhoic <chakhoichan@gmail.com>
2022-11-21 16:45:36 -0500

    1

--- Enumerables.rb
+++ Enumerables.rb
@@ -1,13 +1,11 @@
 class Array
 
-    def
-
-    def my_each(arr)
+    def my_each(&blk)
 
         idx = 0
 
-        while idx < (self.length -1)
-            puts self[idx]
+        while idx < (self.length)
+            blk.call(self[idx])
             idx += 1
         end
 
@@ -30,7 +28,38 @@ class Array
         new_arr
     end
 
-#comment
+    def my_any?(&blk)
+        self.my_each {|ele| return true if blk.call(ele)}
+        false
+    end
+
+    def my_all?(&blk)
+        count = 0
+        self.my_each {|ele| count += 1 if blk.call(ele)}
+
+        if count == self.length
+            return true
+        else
+            false
+        end
+    end
+
+    def my_flatten
+        flatten_arr = []
+        return [self] if !self.is_a?(Array) 
+        self.my_each do |ele|
+            if ele.is_a?(Array)
+                flatten_arr += ele.my_flatten
+            else
+                flatten_arr << ele
+            end
+        end
+        flatten_arr
+    end
+
+   p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
+
+
 
 
 
