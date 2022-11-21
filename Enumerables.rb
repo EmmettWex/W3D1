d746b0decbda69bc790d2aaf73941d21a496728f
“Emmett <emmett.wechsler11@gmail.com>
2022-11-21 17:19:38 -0500

    Adding files to repo

--- Enumerables.rb
+++ Enumerables.rb
@@ -1,4 +1,7 @@
+# require 'Byebug'
+
 class Array
+
 
     def my_each(&blk)
 
@@ -57,10 +60,35 @@ class Array
         flatten_arr
     end
 
-   p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
+#     def my_zip(*arr)
+#         new_arr = Array.new(self.length) { Array.new((arr.length+1),nil) }
+#         arr.push(self)
 
+#         # [ [1,2] , [1,2,3] , [4,5,6] , [7,8,9] , [10,11,12] , [13,14,15] ]
+#         # [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+#         debugger
+#         self.my_each.with_index do |ele, idx1|
 
+#             arr.my_each.with_index do |sub_arr, idx2|
 
+#                 new_arr[idx2][idx1] == arr[idx2][idx1]
+
+#             end
+#         end
+
+#         new_arr
+        
+#     end
+
+# a = [ 4, 5, 6 ]
+# b = [ 7, 8, 9 ]
+# p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+# p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
 
 end
